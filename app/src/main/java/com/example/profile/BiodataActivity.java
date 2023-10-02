3ed1205a73eb24b6f9f0d18e09f4a3aa318a4337
irhmdhn <irhmdhn.me@gmail.com>
2023-10-02 17:05:06 +0700

    Toast, Alert, add activity data

--- app/src/main/java/com/example/profile/BiodataActivity.java
+++ app/src/main/java/com/example/profile/BiodataActivity.java
@@ -3,12 +3,47 @@ package com.example.profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BiodataActivity extends AppCompatActivity {
+
+    TextView tvNamaMhs, tvNimMhs;
+    EditText etProdiMhs;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biodata);
+
+        tvNamaMhs= findViewById(R.id.tvNamaMhs);
+        tvNimMhs= findViewById(R.id.tvNimMhs);
+        etProdiMhs= findViewById(R.id.etProdiMhs);
+        btnSubmit= findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String nama = tvNamaMhs.getText().toString();
+                    String nim = tvNimMhs.getText().toString();
+                    String prodi = etProdiMhs.getText().toString();
+
+                    if(prodi.isEmpty()){
+                        Toast.makeText(getApplication(),"Prodi harus di isi", Toast.LENGTH_SHORT).show();
+                    }else {
+
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(),"ERROR, TRY AGAIN", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/profile/FormActivity.java
+++ app/src/main/java/com/example/profile/FormActivity.java
@@ -1,14 +1,63 @@
 package com.example.profile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class FormActivity extends AppCompatActivity {
+
+    Button btnSimpan, btnHapus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        btnSimpan = findViewById(R.id.btnSimpan);
+        btnHapus = findViewById(R.id.btnHapus);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormActivity.this, "Data Disimpan", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(FormActivity.this);
+                alertDialogBuilder.setTitle("Peringatan");
+                alertDialogBuilder.setMessage("Apakah anda yakin untuk menghapus data")
+                        .setIcon(R.drawable.ic_avatar)
+                        .setCancelable(false)
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(),"Data berhasil dihapus",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+
+                // DIALOG
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+
+            }
+        });
+
+
     }
 }
